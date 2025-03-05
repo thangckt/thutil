d4b8c422b31171d7713b585a9aac23c6e553486a
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2025-03-06 01:24:19 +0900

    Update stuff.py

--- thutil/stuff.py
+++ thutil/stuff.py
@@ -2,7 +2,7 @@ import base64
 import random
 import time
 from typing import Generator
-
+import string
 
 def chunk_list(input_list: list, n: int) -> Generator:
     """Yield successive n-sized chunks from `input_list`."""
@@ -48,9 +48,15 @@ def text_color(text: str, color: str = "blue") -> str:
     return text
 
 
-def uui_time() -> str:
-    timestamp = int(time.time() * 1000)
+def time_uuid() -> str:
+    timestamp = int(time.time() * 1.0e6)
     rand = random.getrandbits(10)
     unique_value = (timestamp << 10) | rand  # Combine timestamp + random bits
     text = base64.urlsafe_b64encode(unique_value.to_bytes(8, "big")).decode().rstrip("=")
     return text.replace("-", "_")
+
+def simple_uuid():
+    """Generate a simple random UUID of 4 digits."""
+    rnd_letter = random.choice(string.ascii_uppercase) # ascii_letters
+    rnd_num = random.randint(100, 999)
+    return f"{rnd_letter}{rnd_num}"
